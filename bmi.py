877790f33dba7720cd71913da0963be1431cbf79
Lewis Yang <xoxo50214l@hotmail.com>
2021-05-27 21:43:32 +0800

    a simple BMI calculator

---
+++ bmi.py
@@ -0,0 +1,19 @@
+h = input('請輸入身高(m): ')
+w = input('請輸入體重(kg): ')
+h = float(h)
+w = float(w)
+bmi = w / (h ** 2)
+print('您的BMI為: ', bmi)
+
+if bmi < 18.5:
+	print('體重過輕!')
+elif 18.5 <= bmi < 24:
+	print('正常範圍')
+elif 24 <= bmi < 27:
+	print('過重!')
+elif 27 <= bmi < 30:
+	print('輕度肥胖!!')
+elif 30 <= bmi < 35:
+	print('中度肥胖!!!')
+elif bmi >= 35:
+	print('重度肥胖!!!!')
